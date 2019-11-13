f6a8bdbdaac1a6d22c0b07291c5fde892861a857
Kevin Baktiar <afterafx@gmail.com>
2019-11-12 16:56:57 -0800

    finished login and signup logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,7 +7,7 @@ const login = async (request, response, next) => {
   try {
     const user = await User.findOne({ username: request.body.username });
     if (!user) throw new ErrorWithHTTPStatus('User does not exist', 404);
-    const isMatched = await bcrypt.compare(request.body.password, user.passowrd);
+    const isMatched = await bcrypt.compare(request.body.password, user.password);
     if (!isMatched) throw new ErrorWithHTTPStatus('Invalid Login Credentials', 401);
 
     const token = jwt.sign(user.username, process.env.JWT_SECRET);
@@ -22,13 +22,13 @@ const login = async (request, response, next) => {
 const signup = async (request, response, next) => {
   try {
     // hash the password
-    const hashedPassword = await bcrypt.hash(request.body.password);
+    const hashedPassword = await bcrypt.hash(request.body.password, 6);
     const userInfo = new User({
       username: request.body.username,
       password: hashedPassword,
     });
     await User.create(userInfo);
-    request.status(201).send('Signed up!');
+    response.status(201).send('Signed up!');
   } catch (error) {
     console.error(error);
     next(error);
